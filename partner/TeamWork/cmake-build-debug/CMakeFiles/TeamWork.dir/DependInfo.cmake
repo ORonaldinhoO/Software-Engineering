f4cfea78fdd7cfa6555ad04be95b1c9f28b0dd8b
Ray <1010821512@qq.com>
2021-10-25 11:52:49 +0800

    可执行文件第一次运行测试

---
+++ partner/TeamWork/cmake-build-debug/CMakeFiles/TeamWork.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "D:/C++Project/TeamWork/Calculator.cpp" "D:/C++Project/TeamWork/cmake-build-debug/CMakeFiles/TeamWork.dir/Calculator.cpp.obj"
+  "D:/C++Project/TeamWork/Examination.cpp" "D:/C++Project/TeamWork/cmake-build-debug/CMakeFiles/TeamWork.dir/Examination.cpp.obj"
+  "D:/C++Project/TeamWork/main.cpp" "D:/C++Project/TeamWork/cmake-build-debug/CMakeFiles/TeamWork.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ partner/TeamWork/cmake-build-debug/CMakeFiles/TeamWork.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TeamWork.dir/Calculator.cpp.obj"
+  "CMakeFiles/TeamWork.dir/Examination.cpp.obj"
+  "CMakeFiles/TeamWork.dir/main.cpp.obj"
+  "TeamWork.exe"
+  "TeamWork.exe.manifest"
+  "TeamWork.pdb"
+  "libTeamWork.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TeamWork.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
